3f982c141715e6d63c8de55f061938325adddc1c
NazarStf <hworker98@gmail.com>
2023-06-01 16:43:08 +0300

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by NazarStf on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
