a08d601d5dc1005b9b62258dbfd840b155cd9d78
NazarStf <hworker98@gmail.com>
2023-06-01 20:19:20 +0300

    Adding Images to the List

--- Hiking/ContentView.swift
+++ Hiking/ContentView.swift
@@ -13,8 +13,7 @@ struct ContentView: View {
 	
     var body: some View {
 		List(self.hikes, id: \.name) { hike in
-			Text(hike.name)
-				.font(.title)
+			HikeCell(hike: hike)
 		}
     }
 }
@@ -23,4 +22,24 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct HikeCell: View {
+	
+	let hike: Hike
+	
+	var body: some View {
+		HStack {
+			Image(hike.imageURL)
+				.resizable()
+				.frame(width: 100, height: 100)
+				.cornerRadius(15)
+			VStack {
+				Text(hike.name)
+					.font(.headline)
+				Text(String(format: "%.2f miles", hike.miles))
+			}
+			
+		}
+	}
 }
